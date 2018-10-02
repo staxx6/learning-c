220e7e1bdb70f2b639970240fda152af8b74a5c5
Simon Oczadly <staxx@hotmail.de>
2018-10-02 15:32:18 +0200

    init

---
+++ gl/main.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <GL/glew.h>
+#ifdef __APPLE__
+#   include <GLUT/glut.h>
+#else
+#   include <GL/glut.h>
+#endif
+
+static struct {
+    GLuint vertex_buffer, element_buffer;
+    GLuint textures[2];
+
+    /* fields for shader objects ... */
+} g_resources;
+
+static int make_resources(void) {
+    return 1;
+}
+
+static void update_fade_factor(void) {
+
+}
+
+static void render(void) {
+    glClearColor(0.5f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(400, 300);
+    glutCreateWindow("Hello World");
+    glutDisplayFunc(&render);
+    glutIdleFunc(&update_fade_factor);
+
+    glewInit();
+    if(!GLEW_VERSION_2_0) {
+        fprintf(stderr, "OpenGL 2.0 not available\n");
+        return 1;
+    }
+
+
+    if(!make_resources()) {
+        fprintf(stderr, "Failed to load resources\n");
+        return 1;
+    }
+
+    glutMainLoop();
+    return 0;
+}
